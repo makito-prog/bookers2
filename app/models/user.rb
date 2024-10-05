c254b431cf4467576b40fb1e94e602c8c1c99c01
EC2 Default User <ec2-user@ip-172-31-26-198.ap-northeast-1.compute.internal>
2024-10-05 07:42:46 +0000

    Finish function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          
   has_many :books, dependent: :destroy
   
-  validates :name, presence: true, length: { in: 2..20 }
-  validates :introduction, length: { maximum: 140 }
+  validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 
   has_one_attached :profile_image
 
